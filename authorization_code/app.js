bddfaaa82677823d1d54fb647790e634b5ba884e
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-06-20 19:09:33 -0500

    getting there

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '7591ac19c7544c578f984f8b185b811f'; // Your client id
+var client_secret = '6f0776825fb04facb442dc0de89abbf7'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
